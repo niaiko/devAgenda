291505464255265505b9386427c15f3e89d99a84
User <fidyniainachris@gmail.com>
2021-01-14 15:43:24 +0300

    calendrier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,29 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
 import { ExamplesModule } from './examples/examples.module';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
+import { ToastrModule, ToastrService  } from 'ngx-toastr';
+import { NgxFullCalendarModule } from 'ngx-fullcalendar';
+import { FullCalendarModule } from '@fullcalendar/angular';  // for FullCalendar!
+import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin
+import interactionPlugin from '@fullcalendar/interaction'; // a plugin
+
+
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+FullCalendarModule.registerPlugins([ // register FullCalendar plugins
+    dayGridPlugin,
+    interactionPlugin
+  ]);
 
 @NgModule({
     declarations: [
         AppComponent,
-        NavbarComponent
+        NavbarComponent,
+        FooterComponent
     ],
     imports: [
         BrowserAnimationsModule,
@@ -22,9 +37,15 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
         RouterModule,
         AppRoutingModule,
         ComponentsModule,
-        ExamplesModule
+        ExamplesModule,
+        PdfViewerModule,
+        ToastrModule,
+        NgxFullCalendarModule,
+        FullCalendarModule,
     ],
-    providers: [],
+    providers: [
+        ToastrService ,
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
